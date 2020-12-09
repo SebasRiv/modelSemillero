d3e9ae1ffe2b0f12fdb6f5e159ec52bcda0d288e
Sebastian Rivera <sebastian-010198@hotmail.com>
2020-12-09 11:08:22 -0500

    generados getters and setters

--- src/main/java/com/clearminds/srg/dtos/Estudiante.java
+++ src/main/java/com/clearminds/srg/dtos/Estudiante.java
@@ -24,4 +24,28 @@ public class Estudiante {
 		return "Estudiante [id=" + id + ", nombre=" + nombre + ", apellido=" + apellido + "]";
 	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
 }
